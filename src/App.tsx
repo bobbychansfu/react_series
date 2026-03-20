398a1d19073089e8658bd893128274067f9fab62
bobbychansfu <bobbychansfu@gmail.com>
2026-03-20 10:26:07 -0700

    vid2-end: images and hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import "./App.css";
+import logo from "./assets/react.svg";
+import { useState, useEffect, useRef } from "react";
 
-const title = "Random People App";
-const techList = ["React", "TypeScript", "Vite"];
 type Person = {
   id: number;
   name: string;
   age: number;
+  fav: boolean;
+  profilePic?: string;
 };
-const people = [
-  { id: 1, name: "Alice", age: 30 },
-  { id: 2, name: "Bobby", age: 44 },
-  { id: 3, name: "Carol", age: 35 },
-];
 
 function Header({ title }: { title: string }) {
-  return <h1> {title} </h1>;
+  return (
+    <h1>
+      {" "}
+      {title} <img src={logo} alt="logo" className="logo" />{" "}
+    </h1>
+  );
 }
 
 function Footer({ techList }: { techList: string[] }) {
@@ -32,26 +34,129 @@ function Footer({ techList }: { techList: string[] }) {
   );
 }
 
-function Main({ peopleList }: { peopleList: Person[] }) {
+function PersonCard({
+  person,
+  toggleFav,
+}: {
+  person: Person;
+  toggleFav: (id: number) => void;
+}) {
   return (
     <>
-      <ul className="people-list">
-        {peopleList.map((person) => (
-          <li key={person.id}>
-            {person.name} - {person.age} years old
-          </li>
-        ))}
-      </ul>
+      Name: {person.name} <br />
+      Age: {person.age} <br />
+      <img src={person.profilePic} alt="profile" /> <br />
+      <span
+        className="heart"
+        onClick={() => {
+          toggleFav(person.id);
+        }}
+      >
+        {person.fav ? "\u{2764}\u{FE0F}" : "\u{1F90D}"}
+      </span>
       <hr />
     </>
   );
 }
 
+function useFetchRandomUser(trigger: number) {
+  const [peopleList, setPeopleList] = useState<Person[]>([]);
+  const nextIdRef = useRef(1);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (trigger === 0) return;
+
+    async function fetchRandomUser() {
+      try {
+        setLoading(true);
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        const response = await fetch("https://randomuser.me/api/");
+        const data = await response.json();
+        const seed = data.info.seed;
+        const name = data.results[0].name.first;
+        const profilePic = data.results[0].picture.large;
+        const age = data.results[0].dob.age;
+
+        setPeopleList((prev) => [
+          ...prev,
+          {
+            id: nextIdRef.current++,
+            name,
+            profilePic,
+            seed,
+            age,
+            fav: false,
+          },
+        ]);
+      } catch (error) {
+        console.log(`${error}`);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchRandomUser();
+  }, [trigger]);
+  return { peopleList, loading, setPeopleList };
+}
+
+function Main() {
+  /*const people: Person[] = [
+    { id: 1, name: "Alice", age: 30, fav: false },
+    { id: 2, name: "Bobby", age: 44, fav: false },
+    { id: 3, name: "Carol", age: 35, fav: false },
+    { id: 4, name: "David", age: 55, fav: false },
+    { id: 5, name: "Eldon", age: 65, fav: false },
+  ];*/
+  const [numPeople, setNumPeople] = useState(0);
+  const { peopleList, loading, setPeopleList } = useFetchRandomUser(numPeople);
+  /*const [peopleList, setPeopleList] = useState(people);
+  const nextIdRef = useRef(people.length);
+  
+  useEffect(() => {
+    if (numPeople > people.length) {
+      setNumPeople(people.length);
+      nextIdRef.current++;
+      console.log(`next ID: ${nextIdRef.current}`);
+    }
+  }, [numPeople]);
+*/
+  const toggleFav = (id: number) => {
+    setPeopleList((prev) =>
+      prev.map((person) =>
+        person.id === id ? { ...person, fav: !person.fav } : person,
+      ),
+    );
+  };
+  const loadingPerson: Person = {
+    name: "Loading...",
+    age: -1,
+    fav: false,
+    id: -1,
+  };
+  return (
+    <>
+      <div className="people-list">
+        {peopleList.slice(0, numPeople).map((person) => (
+          <PersonCard key={person.id} person={person} toggleFav={toggleFav} />
+        ))}
+        {loading && <PersonCard person={loadingPerson} toggleFav={toggleFav} />}
+      </div>
+      <p>{numPeople}</p>
+      <button onClick={() => setNumPeople((prev) => prev + 1)}>
+        Load Random Person
+      </button>
+    </>
+  );
+}
+
 function App() {
+  const title = "Random People App";
+  const techList = ["React", "TypeScript", "Vite"];
   return (
     <>
       <Header title={title} />
-      <Main peopleList={people} />
+      <Main />
       <Footer techList={techList} />
     </>
   );
